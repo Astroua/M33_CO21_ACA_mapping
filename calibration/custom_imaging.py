07007d40bb0e0d82385260a8004f925d2f8495c3
e-koch <koch.eric.w@gmail.com>
2019-12-18 21:00:33 -0700

    Add script to run apply pipeline calibration for all scheduling blocks

--- calibration/custom_imaging.py
+++ calibration/custom_imaging.py
@@ -52,3 +52,57 @@ for myspw_num in ['20', '22', '24', '26', '28']:
            interactive=0,
            savemodel='none',
            parallel=False)
+
+# Using automasking close to settings in guide:
+# https://casaguides.nrao.edu/index.php/Automasking_Guide
+# I'm having to lower the lownoisethreshold for the
+# masking to find anything.
+
+# Test on first 2019 block (Brick3-Tile1)
+for myspw_num in ['16', '20', '22', '24', '26', '28']:
+
+
+    tclean(vis=['uid___A002_Xe1baa0_X265a.ms'],
+           field='M33', spw=[myspw_num],
+           intent='OBSERVE_TARGET#ON_SOURCE',
+           datacolumn='corrected',
+           imagename='custom_tclean.M33_sci.spw{}.cube_speclim'.format(myspw_num),
+           imsize=[512, 512],
+           cell=['1arcsec'],
+           phasecenter='J2000 01:33:30.50 +030.34.24.70',
+           stokes='I',
+           specmode='cube',
+           nchan=500,
+           start=800,
+           # nchan=100,
+           # start=800,
+           width=1,
+           outframe='LSRK',
+           gridder='mosaic',
+           chanchunks=-1,
+           usepointing=False,
+           mosweight=True,
+           pblimit=0.2,
+           deconvolver='multiscale',
+           scales=[0, 7, 14],
+           restoration=True,
+           restoringbeam='common',
+           pbcor=False,
+           weighting='briggs',
+           robust=0.5,
+           niter=100000,
+           cycleniter=500,  # Force many major cycles
+           nsigma=5.,
+           usemask='auto-multithresh',
+           sidelobethreshold=1.25,
+           noisethreshold=3.0,
+           lownoisethreshold=1.5,
+           negativethreshold=0.0,
+           minbeamfrac=0.1,
+           growiterations=75,
+           dogrowprune=True,
+           minpercentchange=1.0,
+           threshold='0.0mJy',
+           interactive=0,
+           savemodel='none',
+           parallel=False)
--- constants.py
+++ constants.py
@@ -23,3 +23,60 @@ line_dict = {"12co": co21_freq,
 
 co21_mass_conversion = 6.7 * (u.Msun / u.pc ** 2) / (u.K * u.km / u.s)
 beam_eff_30m_druard = 56 / 92.
+
+
+# Define the uid to BrickXTileY conversion. Also SB name.
+
+brick_uid = {"Brick1Tile1": [{'proj': '2019.1.01182.S', 'name': 'M33_l_06_7M',
+                              'memb_ous': 'A001_X1465_X1672',
+                              'sci_ous': 'A001_X1465_X1670'}],
+             "Brick1Tile2": [{'proj': '2019.1.01182.S', 'name': 'M33_k_06_7M',
+                              'memb_ous': 'A001_X1465_X166e ',
+                              'sci_ous': 'NODELIVERY'}],
+             "Brick1Tile3": [{'proj': '2019.1.01182.S', 'name': 'M33_j_06_7M',
+                              'memb_ous': 'A001_X1465_X166a',
+                              'sci_ous': 'A001_X1465_X1668'},
+                             {'proj': '2017.1.00901.S', 'name': 'M33_m_06_7M',
+                              'memb_ous': 'A001/X1296/X6cd ',
+                              'sci_ous': 'A001_X1296_X6cb'}],
+             "Brick1Tile4": [{'proj': '2019.1.01182.S', 'name': 'M33_i_06_7M',
+                              'memb_ous': 'A001_X1465_X1666',
+                              'sci_ous': 'A001_X1465_X1664'}],
+             "Brick1Tile5": [{'proj': '2019.1.01182.S', 'name': 'M33_h_06_7M',
+                              'memb_ous': 'A001_X1465_X1662',
+                              'sci_ous': 'A001_X1465_X1660'}],
+             "Brick2Tile1": [{'proj': '2017.1.00901.S', 'name': 'M33_j_06_7M',
+                              'memb_ous': 'A001_X1296_X6c1 ',
+                              'sci_ous': 'A001_X1296_X6bf'}],
+             "Brick2Tile2": [{'proj': '2019.1.01182.S', 'name': 'M33_i_06_7M',
+                              'memb_ous': 'A001_X1465_X165e',
+                              'sci_ous': 'NODELIVERY'}],
+             "Brick2Tile3": [{'proj': '2019.1.01182.S', 'name': 'M33_f_06_7M',
+                              'memb_ous': 'A001_X1465_X165a',
+                              'sci_ous': 'NODELIVERY'}],
+             "Brick2Tile4": [{'proj': '2019.1.01182.S', 'name': 'M33_e_06_7M',
+                              'memb_ous': 'A001_X1465_X1656',
+                              'sci_ous': 'A001_X1465_X1654'}],
+             "Brick2Tile5": [{'proj': '2019.1.01182.S', 'name': 'M33_d_06_7M',
+                              'memb_ous': 'A001_X1465_X1652',
+                              'sci_ous': 'A001_X1465_X1650'}],
+             "Brick3Tile1": [{'proj': '2019.1.01182.S', 'name': 'M33_c_06_7M',
+                              'memb_ous': 'A001_X1465_X164e',
+                              'sci_ous': 'A001_X1465_X164c'}],
+             "Brick3Tile2": [{'proj': '2019.1.01182.S', 'name': 'M33_b_06_7M',
+                              'memb_ous': 'A001_X1465_X164a',
+                              'sci_ous': 'A001_X1465_X1648'}],
+             "Brick3Tile3": [{'proj': '2019.1.01182.S', 'name': 'M33_a_06_7M',
+                              'memb_ous': 'A001_X1465_X1646',
+                              'sci_ous': 'NODELIVERY'}],
+             "Brick3Tile4": [{'proj': '2017.1.00901.S', 'name': 'M33_b_06_7M',
+                              'memb_ous': 'A001_X1296_X6a1',
+                              'sci_ous': 'A001_X1296_X69f'}],
+             "Brick3Tile5": [{'proj': '2017.1.00901.S', 'name': 'M33_a_06_7M',
+                              'memb_ous': 'A001_X1296_X69d',
+                              'sci_ous': 'A001_X1296_X69b'}]}
+
+# casa versions to run pipeline for different projects.
+
+casa_pipeline_versions = {'2017.1.00901.S': 'casa-release-5.4.0-68.el6',
+                          '2019.1.01182.S': 'casa-pipeline-release-5.6.1-8.el7'}

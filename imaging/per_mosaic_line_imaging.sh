00ab92498dbdeab8df02032b7a1d3cb0aeff2773
e-koch <koch.eric.w@gmail.com>
2020-01-08 17:46:14 -0700

    Change loop order and run 5 image jobs at once

--- imaging/per_mosaic_line_imaging.sh
+++ imaging/per_mosaic_line_imaging.sh
@@ -12,21 +12,43 @@ mkdir ${data_path}/per_mosaic_imaging/logs/
 cd ${data_path}/per_mosaic_imaging/logs/
 
 lines=( "12CO21" "13CO21" "C18O21" "H2CO_303_202" "H2CO_322_221" "H2CO_321_220" )
-spec_widths=( "native" "0p7kms" "1p3kms" "2kms" "2p6kms" )
+spec_widths=( "0p7kms" "1p3kms" "2kms" "2p6kms" "native")
 
 # rerun_existing=True
 rerun_existing=False
 
-for brick_num in {1..3}; do
-    for tile_num in {1..5}; do
-        for line in "${lines[@]}"; do
-            for spec_width in "${spec_widths[@]}"; do
+
+# for spec_width in "${spec_widths[@]}"; do
+#     for line in "${lines[@]}"; do
+#         for brick_num in {1..3}; do
+#             for tile_num in {1..5}; do
+
+#                 logfile_name="casa_M33_ACA_Brick${brick_num}Tile${tile_num}_${line}_${spec_width}_$(date "+%Y%m%d-%H%M%S").log"
+
+#                 ${casa_path} --logfile ${data_path}/${logfile_name} -c ${code_path}/per_mosaic_line_imaging.py ${brick_num} ${tile_num} ${line} ${spec_width} ${rerun_existing}
+
+#             done
+#         done
+#     done
+# done
+
+for spec_width in "${spec_widths[@]}"; do
+    for line in "${lines[@]}"; do
+        for brick_num in {1..3}; do
+            # Run all 5 tiles at once.
+            pids=
+            for tile_num in {1..5}; do
 
                 logfile_name="casa_M33_ACA_Brick${brick_num}Tile${tile_num}_${line}_${spec_width}_$(date "+%Y%m%d-%H%M%S").log"
 
-                ${casa_path} --logfile ${data_path}/${logfile_name} -c ${code_path}/per_mosaic_line_imaging.py ${brick_num} ${tile_num} ${line} ${spec_width} ${rerun_existing}
+                (${casa_path} --nologger --nogui --log2term --nocrashreport --logfile ${data_path}/${logfile_name} -c ${code_path}/per_mosaic_line_imaging.py ${brick_num} ${tile_num} ${line} ${spec_width} ${rerun_existing}) &
+                pids+=" $!"
 
+                sleep 20
             done
+
+            wait $pids || {echo "There was an error" >&2}
+
         done
     done
 done
